d34219541b7e25935bed68b02f28cc2e9c5e4876
Ivan Maloshtan <maloshtan.ivan@gmail.com>
2021-12-15 01:29:04 +0200

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,15 +15,17 @@ interface Props{
     closeForm: ()=> void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props){
+export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting}: Props){
     return(
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} 
                     selectActivity={selectActivity}
                     deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -36,7 +38,12 @@ export default function ActivityDashboard({activities, selectedActivity, selectA
                 {/* строка ниже создает форму,которая описана в ActivityForm */}
                 {editMode &&
                 // createOrEdit вносит реальные изменения в форму (они сохранятся ЛОКАЛЬНО, а потом будут отображаться ЛОКАЛЬНО-т.е. после обновы странички все уберется )
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}
+                />}
             </Grid.Column>
         </Grid>
     )
