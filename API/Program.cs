129ad3790b82586083303d74fde76972c581432a
Ivan Maloshtan <maloshtan.ivan@gmail.com>
2023-10-31 19:03:06 +0100

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
